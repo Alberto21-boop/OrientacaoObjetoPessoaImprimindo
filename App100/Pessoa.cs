ff1b176011c1b52adbae8a10bc059432a519aad1
alberto <albertobarbosa0003@gmail.com>
2022-04-22 14:12:29 -0300

    Adicionar arquivos de projeto.

---
+++ App100/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App100
+{
+    internal class Pessoa
+    {
+        public string GetNome { get; set; } = default!;
+        public int GetIdade { get; set; }
+        public string GetProfissao { get; set; } = default!;
+
+        public string Nome()
+        {
+            return GetNome;
+        }
+        public int Idade()
+        {
+            return GetIdade;
+        }
+        public string Profissao()
+        {
+            return GetProfissao;
+        }
+    }
+}
---
+++ App100/PessoaCompleta.cs
@@ -0,0 +1,18 @@
+﻿using App100;
+using System.IO;
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.GetNome = Console.ReadLine();
+pessoa.GetIdade = int.Parse(Console.ReadLine());
+pessoa.GetProfissao = Console.ReadLine();
+
+string[] lines = { "Nome : " + pessoa.GetNome, "Idade : " + pessoa.GetIdade, "Profissão : " + pessoa.GetProfissao };
+
+string docPath = @"C:\Users\Àlberto Barbòsa\Desktop\PastaImprime";
+
+using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+{
+    foreach (string line in lines)
+        outputFile.WriteLine(line);
+}
